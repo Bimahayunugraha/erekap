38e412bae9bac6eb549c072209893a5d3f4e5340
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-11-06 14:37:27 +0700

    feat/guru: added modal edit guru component for guru page

--- src/components/Admin/GuruListItem.jsx
+++ src/components/Admin/GuruListItem.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
+import ModalEditGuru from "./ModalEditGuru";
 
 const GuruListItem = () => {
+	const [modalEditTrigger, setModalEditTrigger] = useState(false);
+
+	const handleModalEditTrigger = () => {
+		setModalEditTrigger(!modalEditTrigger);
+	};
+
 	return (
 		<tbody>
 			<tr className="border-b bg-white hover:bg-gray-50">
-				<th scope="row" className="whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white">
+				<th scope="row" className="whitespace-nowrap py-4 px-6 font-semibold text-gray-900 dark:text-white">
 					Puji Astudi S.Pd.
 				</th>
-				<td className="py-4 px-6">197689019210212</td>
-				<td className="py-4 px-6">Perempuan</td>
-				<td className="py-4 px-6">081901901901</td>
+				<td className="py-4 px-6 font-medium">197689019210212</td>
+				<td className="py-4 px-6 font-medium">Perempuan</td>
+				<td className="py-4 px-6 font-medium">081901901901</td>
 				<td className="py-4 px-6">
 					<div className="flex items-center space-x-4 text-sm">
-						<button type="button" className="font-medium text-blue-600 hover:underline">
+						<button type="button" className="font-medium text-blue-600 hover:underline" onClick={handleModalEditTrigger}>
 							<PencilIcon className="h-6 w-6 text-blue-600 transition duration-75 hover:text-blue-700" />
 						</button>
 						<button type="button" className="font-medium text-blue-600 hover:underline">
 							<TrashIcon className="h-6 w-6 text-red-400 transition duration-75 hover:text-red-500" />
 						</button>
 					</div>
+					{modalEditTrigger && <ModalEditGuru handleModalEditTrigger={handleModalEditTrigger} />}
 				</td>
 			</tr>
 		</tbody>
