296e19218755bbb935055b4def7d5dc700aac2ce
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-11-06 19:33:33 +0700

    feat/hasura_graphql: configure hasura graphql with apollo client and graphql ws

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { ApolloProvider } from "@apollo/client";
 import { Provider } from "react-redux";
 import "./App.css";
+import client from "./graphqls";
 import SetupRouter from "./routes";
 import { store } from "./stores";
 
 function App() {
 	return (
 		<>
+			<ApolloProvider client={client}></ApolloProvider>
 			<Provider store={store}>
 				<SetupRouter />
 			</Provider>
