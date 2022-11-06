9044370e5cec5ce9adf3c8c8900c1e8196a4f0da
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-11-06 12:26:02 +0700

    feat/dashboard: added drawer component

--- src/components/General/Header.jsx
+++ src/components/General/Header.jsx
@@ -6,16 +6,16 @@ import icon from "../../assets/img/svg/Icon.svg";
 import iconAvatar from "../../assets/img/png/icon-avatar.png";
 import Dropdown from "./Dropdown";
 
-const Header = () => {
-	const [modalTrigger, setModalTrigger] = useState(false);
-	const handleModalTrigger = () => {
-		setModalTrigger(!modalTrigger);
+const Header = ({ handledrawerTrigger }) => {
+	const [dropdownTrigger, setdropdownTrigger] = useState(false);
+	const handledropdownTrigger = () => {
+		setdropdownTrigger(!dropdownTrigger);
 	};
 
 	return (
 		<header className="fixed top-0 left-0 flex h-16 w-full items-center justify-between border-b border-gray-200 bg-white px-4 py-2 md:px-8">
 			<div className="flex w-2/3 items-center">
-				<div className="cursor-pointer rounded-full p-2 hover:bg-gray-200 md:hidden">
+				<div className="cursor-pointer rounded-full p-2 hover:bg-gray-200 md:hidden" onClick={handledrawerTrigger}>
 					<Bars3Icon className="h-6 w-6 text-gray-800" />
 				</div>
 				<Link to="/dashboard" className="flex items-center pl-2 md:hidden">
@@ -27,12 +27,12 @@ const Header = () => {
 				<div className="relative">
 					<button
 						className="relative flex h-10 w-10 cursor-pointer items-center justify-center overflow-hidden rounded-full bg-gray-100"
-						onClick={handleModalTrigger}>
+						onClick={handledropdownTrigger}>
 						<span className="font-medium text-gray-600">
 							<img src={iconAvatar} alt="avatar" />
 						</span>
 					</button>
-					{modalTrigger && <Dropdown />}
+					{dropdownTrigger && <Dropdown />}
 				</div>
 			</div>
 		</header>
