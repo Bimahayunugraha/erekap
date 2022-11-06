35f55d1242128df27a2b5efdf572df8f5e6ff40e
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-11-06 14:58:39 +0700

    feat/pelajaran_diampu: added pelajaran diampu list for pelajaran diampu page

---
+++ src/components/Admin/PelajaranDiampuList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { PlusIcon } from "@heroicons/react/24/outline";
+
+const PelajaranDiampuList = () => {
+	return (
+		<div className="container mx-auto py-6 px-2">
+			<div className="flex flex-wrap">
+				<div className="w-full px-4 lg:w-1/2 xl:w-1/5">
+					<Link to="/admin/tambah/pelajaran">
+						<div className="mb-10 cursor-pointer rounded-md border border-gray-100 p-10 text-center">
+							<div className="inline-block items-center justify-center">
+								<PlusIcon className="h-6 w-6 text-gray-600" />
+							</div>
+							<p className="text-xs font-medium leading-7 text-gray-600">Tambah pelajaran diampu</p>
+						</div>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default PelajaranDiampuList;
--- src/pages/Admin/PelajaranDiampuPage.jsx
+++ src/pages/Admin/PelajaranDiampuPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PelajaranDiampuList from "../../components/Admin/PelajaranDiampuList";
 
 const PelajaranDiampuPage = () => {
 	return (
@@ -6,6 +7,7 @@ const PelajaranDiampuPage = () => {
 			<div className="flex items-center justify-between border-b border-gray-200 bg-white px-6 py-3">
 				<h2 className="text-xl font-bold text-gray-800">Data Pelajaran Diampu</h2>
 			</div>
+			<PelajaranDiampuList />
 		</div>
 	);
 };
