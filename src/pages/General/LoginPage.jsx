e0ee1d89d247c3235662f02ec638572cb7d2e7b7
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-11-06 18:13:29 +0700

    feat/auth: implementation auth login with firebase

--- src/pages/General/LoginPage.jsx
+++ src/pages/General/LoginPage.jsx
@@ -1,8 +1,102 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 import icon from "../../assets/img/svg/Icon.svg";
+import Login from "../../hooks/useLogin";
+import { PulseLoader } from "react-spinners";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+
+const Alert = MySwal.mixin({
+	toast: false,
+	showConfirmButton: true,
+	customClass: {
+		container: "custom-toast",
+	},
+});
 
 const LoginPage = () => {
+	const baseValue = {
+		email: "",
+		password: "",
+		role: "",
+	};
+
+	const baseErrors = {
+		email: "",
+	};
+
+	const [values, setValues] = useState(baseValue);
+	const [errors, setErrors] = useState(baseErrors);
+	const [loading, setLoading] = useState(false);
+
+	const navigate = useNavigate();
+	const login = Login();
+
+	const regexEmailValidation =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setValues({
+			...values,
+			[name]: value,
+		});
+
+		validation(name, value);
+	};
+
+	const validation = (name, value) => {
+		if (name === "email") {
+			if (!regexEmailValidation.test(value)) {
+				setErrors({ ...errors, email: "Email tidak sesuai" });
+			} else {
+				setErrors({ ...errors, email: "" });
+			}
+		}
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setLoading(true);
+
+		try {
+			await login(values.email, values.password, values.role);
+			setTimeout(() => navigate("/dashboard"), 2000);
+			setLoading(false);
+		} catch (error) {
+			if (error.code === "auth/wrong-password") {
+				setTimeout(
+					() =>
+						Alert.fire({
+							icon: "error",
+							title: <strong>Error</strong>,
+							text: "Password salah",
+							background: "#fef2f2",
+							confirmButtonColor: "#fecaca",
+						}),
+					1000
+				);
+			}
+			if (error.code === "auth/user-not-found") {
+				setTimeout(
+					() =>
+						Alert.fire({
+							icon: "error",
+							title: <strong>Error</strong>,
+							text: "Email belum terdaftar",
+							background: "#fef2f2",
+							confirmButtonColor: "#fecaca",
+						}),
+					1000
+				);
+			}
+			setLoading(false);
+		}
+	};
+
 	return (
 		<div className="mx-auto flex h-screen flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
 			<div className="w-full rounded-lg bg-white shadow dark:border sm:max-w-md md:mt-0 xl:p-0">
@@ -11,7 +105,7 @@ const LoginPage = () => {
 				</Link>
 				<div className="space-y-4 p-6 sm:p-8 md:space-y-6">
 					<h1 className="pb-2 text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">Login</h1>
-					<form className="space-y-4 md:space-y-6">
+					<form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
 						<div>
 							<label htmlFor="email" className="mb-2 block text-sm font-medium text-gray-900">
 								<span className="block after:ml-1 after:text-red-500 after:content-['*']">Email</span>
@@ -24,7 +118,10 @@ const LoginPage = () => {
 								placeholder="Masukkan email"
 								required
 								autoFocus
+								onChange={handleChange}
+								value={values.email}
 							/>
+							{errors.email && <span className="mt-2 block text-sm text-red-700">{errors.email}</span>}
 						</div>
 						<div>
 							<label htmlFor="password" className="mb-2 block text-sm font-medium text-gray-900">
@@ -37,13 +134,23 @@ const LoginPage = () => {
 								placeholder="Masukkan password"
 								className="block w-full rounded-lg border border-gray-300 p-2.5 text-gray-900 placeholder-gray-500 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
 								required
+								onChange={handleChange}
+								value={values.password}
 							/>
 						</div>
-						<button
-							type="submit"
-							className="w-full rounded-lg bg-indigo-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-4 focus:ring-indigo-300">
-							Login
-						</button>
+						{loading ? (
+							<button
+								className="w-full rounded-lg bg-indigo-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-4 focus:ring-indigo-300"
+								disabled={values}>
+								<PulseLoader size={7} color={"#ffffff"} />
+							</button>
+						) : (
+							<button
+								type="submit"
+								className="w-full rounded-lg bg-indigo-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-4 focus:ring-indigo-300">
+								Login
+							</button>
+						)}
 						<p className="text-center text-sm font-light text-gray-500 ">
 							Belum memiliki akun?
 							<Link to="/register" className="ml-1 font-medium text-indigo-600 hover:underline">
